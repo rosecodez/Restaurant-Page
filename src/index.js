acb7c75f9182789d9dec987d7dbbc2acfca5202f
Rose <rosebeats09@gmail.com>
2023-09-25 18:07:46 -0400

    Change webpack files, images and add index boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import './style.css';
+//import Icon from './icon.png';
+//import Icon from './background-image.avif';
+
+let content = document.getElementById("content");
+function createEl(div) {
+    return document.createElement("div");
+}
+createEl("div").content.appendChild("div");
