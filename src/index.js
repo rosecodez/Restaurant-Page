b657cbc7678f36afeeaf1dfe46426778b3f78e9b
Rose <rosebeats09@gmail.com>
2023-09-27 20:33:52 -0400

    Loop through array for li and assign text content for them

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import _ from 'lodash';
+import _, { last } from 'lodash';
 import './style.css';
-//import Icon from './icon.png';
-//import Icon from './background-image.avif';
+import img from './logo.png';
+
 
 let content = document.getElementById("content");
-
-function createBtn() {
-    document.createElement("button");
-}
-function createElement() {
+function createElements() {
     // ol id="header"
     const ol = document.createElement("ol");
     content.appendChild(ol);
@@ -20,11 +16,20 @@ function createElement() {
         ol.appendChild(li);
         let a = document.createElement("a");
         li.appendChild(a);
-        liArray.push(i);
-        console.log(liArray); 
+        liArray.push(li);
     }
-    // create button
-    const button = createBtn();
+    for (let i = 0; i < liArray.length; i++) {
+        console.log(liArray[i]);
+    }
+
+    liArray[1].textContent = "Home";
+    liArray[2].textContent = "Menu";
+    liArray[3].textContent = "Contact";
+    liArray[4].textContent = "Join us";
+    const button = document.createElement("button");
+    liArray[5].appendChild(button);
+    button.textContent = "Reservation";
+
     // div id="main"
     const main = document.createElement("div");
     content.append(main);
@@ -32,21 +37,18 @@ function createElement() {
         // div id="background-image"
         const backgroundImg = document.createElement("div");
         main.appendChild(backgroundImg);
+
         backgroundImg.setAttribute("id", "background-image");
             const imgTxt = document.createElement("div");
             backgroundImg.appendChild(imgTxt);
             imgTxt.setAttribute("id", "image-text");
-                const logo = document.createElement("img");
-                imgTxt.appendChild(logo);
-                logo.setAttribute("id", "logo");
 
                 const header = document.createElement("h1");
                 header.setAttribute("id", "header");
                 header.textContent = "Sushi Workshop";
-                
-    const array = [];
-    array.push(ol, );
-    return array;
 
+    const array = [];
+    array.push(ol);
+    return array;
 }
-console.log(createElement());
+console.log(createElements());
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
-    ],
-  },
-};
+ module.exports = {
+   entry: './src/index.js',
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   module: {
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+            name: '[name].[ext]',
+        },
+    }
+     ],
+   },
+ };
